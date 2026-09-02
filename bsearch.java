02dd666d7479b56dfa2818391fc83ed45d5911f9
vijaykumar-prog <kumar386493@gmail.com>
2026-09-02 20:10:01 +0530

    binary search in 2D array

---
+++ bsearch.java
@@ -0,0 +1,37 @@
+public class bsearch {
+    public static void main(String[] arg){
+        int nums[][] = { {1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}};
+
+        int target = 4;
+        int n = nums.length;
+        int m = nums[0].length;
+        int low=0;
+        int high = n*m -1;
+        int iteration =0;
+
+        while(low<=high){  
+            iteration++;
+            int mid = (low+high)/2;
+
+            int row = mid / m;
+            int col = mid % m;
+
+            if(nums[row][col] == target){
+                System.out.println(row+" "+col);break;
+            }
+            else if(nums[row][col]<target){
+                low = mid + 1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        System.out.println(iteration);
+
+        
+    }
+    
+}
---
+++ twoD.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class twoD {
+    public static void main(String[] args){
+        
+         int[][] nums = {
+            {12,23,45,67},
+            {15,24,49,73},
+            {17,26,52,87},
+            {25,29,58,99}
+        };
+        Scanner in = new Scanner(System.in);
+        int target =in.nextInt();
+        int index1 = 0;
+        int index2 = 0;
+        boolean found = false;
+
+        for(int i=0;i<nums.length;i++){
+            int row = 0;
+            int col = nums.length -1;
+            
+
+            while(row <= col){
+                int mid = (col+row)/2;
+
+                if(nums[i][mid]== target){
+                    index1 = i;
+                    index2 = mid;
+                    found = true;
+                    break;
+                }
+                else if(nums[i][mid]<target){
+                    
+                    row = mid + 1;
+                }
+                else{
+                    col = mid -1;
+                }
+            }
+        }
+        if(found){
+            System.out.println(index1+","+index2);
+        }
+        else{
+            System.out.println("Element not found");
+        }
+        in.close();
+        
+
+    }
+    
+}
