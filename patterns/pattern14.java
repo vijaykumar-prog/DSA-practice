d53da769a8f853b9a90f28635cecea2016ad472d
vijaykumar-prog <kumar386493@gmail.com>
2026-09-04 20:56:53 +0530

    right angled alphabet triangle

---
+++ patterns/pattern14.java
@@ -0,0 +1,15 @@
+public class pattern14 {
+    public static void main(String[] arg){
+        int n=5;
+        
+        for(int i=0;i<n;i++){
+            char ch ='A';
+            for(int j=0;j<=i;j++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+    
+}
