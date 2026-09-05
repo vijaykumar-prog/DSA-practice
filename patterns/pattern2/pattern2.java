8016a48042178460fce836438855ec2de3eeead0
vijaykumar-prog <kumar386493@gmail.com>
2026-09-05 12:48:46 +0530

    pattern2

--- patterns/pattern2/pattern2.java
+++ patterns/pattern2/pattern2.java
@@ -1,12 +1,14 @@
+package pattern2;
 public class pattern2 {
-    class Solution {
-    public void pattern2(int n) {
-        for(int i=0;i<n;i++){
+   public static void main(String[] arg){
+        int n=5;
+
+      for(int i=0;i<n;i++){
             for(int j=0;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+   }
+      
     }
-}
-}
